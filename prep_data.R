e5d7a9b9deecfcec4157e350ae469658449d58bc
volker-4011 <volker.paatz@gmx.de>
2021-05-24 17:07:04 +0200

    Einfügen Variablen Volker

--- prep_data.R
+++ prep_data.R
@@ -23,7 +23,9 @@
     source("https://raw.githubusercontent.com/volker-4011/Gruppe_3/main/ferientage.R", encoding = "UTF-8")
     source("https://raw.githubusercontent.com/volker-4011/Gruppe_3/main/feiertage.R", encoding = "UTF-8")
     
-    #Bearbeiten der Daten###############
+    #####################################################
+    
+    #Bearbeiten von wetter_dwd
     wetter_dwd$MESS_DATUM <- as.Date(wetter_dwd$MESS_DATUM, "%d.%m.%Y")
     wetter_dwd <- dplyr::rename(wetter_dwd, Datum = MESS_DATUM)
     wetter_dwd[wetter_dwd==-999.000] <- NA
@@ -45,10 +47,22 @@
                     'eor'
     )] <- list(NULL)
     
+    #Konvertieren der hinzugefügten Spalten aus wetter_dwd von "Character" zu "numeric"
+    wetter_dwd$Sonnenscheindauer <- as.numeric(wetter_dwd$Sonnenscheindauer)
+    wetter_dwd$Relative_Feuchte <- as.numeric(wetter_dwd$Relative_Feuchte)
+    wetter_dwd$Niederschlagsmenge <- as.numeric(wetter_dwd$Niederschlagsmenge)
+    
+    ####################################
+
+    ###Vorbereitung für die Vorhersage: Zeilen für alle 6 Warengruppen hinzufügen für den ersten Tag, für den keine Umsatzdaten vorhanden sind
+    d <- max(umsatzdaten$Datum)+1
+    for(i in 1:6){
+      umsatzdaten <- add_row(umsatzdaten, Datum = d, Warengruppe = i)
+    }
+    
     ####################################
     
-
-    #Zusammensetzen der Daten
+    ###Zusammensetzen der Daten
     fullData <- merge(umsatzdaten,wetter, by="Datum", all.x = TRUE)
     fullData <- merge(fullData,kiwo, by="Datum", all.x = TRUE)
     fullData <- merge(fullData,wetter_dwd, by="Datum", all.x = TRUE)
@@ -56,14 +70,36 @@
     fullData <- merge(fullData,feiertage, by="Datum", all.x = TRUE)
     #########################
     
+    ###Optimierung der Daten/ Hinzufügen neuer Variablen
     
-    #Vorschlag: Optimierung der Daten
+    #Windchillfaktor berechnen und hinzufügen (gefühlte Temperatur für unteren Temperaturbereich)
+    fullData$Windchill <- with(fullData, 13.12+0.6215*Temperatur+(0.3965*Temperatur-11.37)*Windgeschwindigkeit^0.16)
     
-    fullData$Umsatz <- round(fullData$Umsatz) #Dezimalstellen wenig sinvoll, besser Runden
-    fullData$KielerWoche[is.na(fullData$KielerWoche)] <- 0 #Entweder Kieler-Woche oder "nicht = 0"
+    #Extrahieren des Wochentags aus dem Datum und speichern in neuer Variablen
+    fullData$Wochentag <- weekdays(fullData$Datum)
+    
+    #Extrahieren des Monats aus dem Datum und speichern in neuer Variablen
+    fullData$Monat <- month(fullData$Datum)
+    
+    #Hinzufügen neue Variable Wochenende
+    fullData$Wochenende <- with(fullData, ifelse(Wochentag=="Sonntag" | Wochentag=="Samstag", 1, 0))
+    
+    #Dezimalstellen wenig sinvoll, besser Runden
+    fullData$Umsatz <- round(fullData$Umsatz)
+    
+    #Alle NA durch 0 ersetzen in Boolschen Variablen
+    fullData$KielerWoche[is.na(fullData$KielerWoche)] <- 0 # Alle NA in KielerWoche durch 0 ersetzen
     fullData$Ferien[is.na(fullData$Ferien)] <- 0 #Entweder Ferien oder "nicht = 0"
     fullData$Feiertag[is.na(fullData$Feiertag)] <- 0 #Entweder Feiertag oder "nicht = 0"
     
-    #Vorschlag: Optimierung der Daten 
+    #########################
+    
+    #Dataframe für nächsten Tag erstellen (für die Vorhersage)  
+    
+    #Abspeichern der Daten für den Tag, der vorhergesagt werden soll in einem neuen Dataframe
+    newData <- fullData[fullData$Datum == d, ]
+    
+    #Löschen der Daten für den Tag, der vorhergesagt werden soll aus den Trainingsdaten
+    fullData <- subset(fullData, Datum != d)
 
   ####################################################################################################################################
